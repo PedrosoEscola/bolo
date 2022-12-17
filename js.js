ce58686a8ef136f7fce287c0855f5304721a1720
Pedro Henrique <115383774+PedrosoEscola@users.noreply.github.com>
2022-12-17 18:09:16 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function clicou() {
+  let presente = document.getElementById('presente')
+  presente.setAttribute('src', 'bolo.jpg')
+  let text = document.getElementById('texto')
+  text.innerText = 'O bolo ficou pronto'
+}
